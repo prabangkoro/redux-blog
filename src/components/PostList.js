88681f7f3c4e8fd1a1089f5dd275fd4d3482ab9e
Bagus Prabangkoro (babang) <bagus.prabangkoro@gdn-commerce.com>
2019-09-17 19:19:48 +0700

    add actions and lifecycle method

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-export default class PostList extends Component {
+class PostList extends Component {
+  componentDidMount () {
+    this.props.fetchPosts()
+  }
+
   render() {
     return (
       <div>
@@ -9,3 +15,5 @@ export default class PostList extends Component {
     )
   }
 }
+
+export default connect(null, { fetchPosts })(PostList)
